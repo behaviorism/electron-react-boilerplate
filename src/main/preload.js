5d575f5869eeb836c25578b36e4bc23e5ea2a0e8
K <59264215+behaviorism@users.noreply.github.com>
2023-10-19 08:51:32 +0200

    Update preload.js

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,3 +1,12 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("ipc", ipcRenderer);
+contextBridge.exposeInMainWorld("ipc", {
+  send: (channel, ...args) => ipcRenderer.send(channel, ...args),
+  invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
+  on: (channel, listener) => ipcRenderer.on(channel, listener),
+  once: (channel, listener) => ipcRenderer.once(channel, listener),
+  addEventListener: (channel, listener) =>
+    ipcRenderer.addListener(channel, listener),
+  removeListener: (channel, listener) => ipcRenderer.removeListener(channel, listener),
+  removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel),
+});
