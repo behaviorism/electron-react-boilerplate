1089ce94b888879a733ebd4ed2ea41a5e541f1c0
K <59264215+behaviorism@users.noreply.github.com>
2023-10-19 08:52:17 +0200

    Update preload.d.ts

--- src/renderer/preload.d.ts
+++ src/renderer/preload.d.ts
@@ -1,6 +1,14 @@
 declare global {
   interface Window {
-    ipc: Electron.IpcRenderer;
+    ipc: {
+      send: typeof Electron.ipcRenderer.send;
+      invoke: typeof Electron.ipcRenderer.invoke;
+      on: typeof Electron.ipcRenderer.on;
+      once: typeof Electron.ipcRenderer.once;
+      addEventListener: typeof Electron.ipcRenderer.addListener;
+      removeListener: typeof Electron.ipcRenderer.removeListener;
+      removeAllListeners: typeof Electron.ipcRenderer.removeAllListeners;
+    };
   }
 }
 
